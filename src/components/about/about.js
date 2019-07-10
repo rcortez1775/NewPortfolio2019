d5fc404cf9715b73dc4953bc78b7024c18478fc8
Robert Cortez <Nuhuhfoo@gmail.com>
2019-07-10 08:29:10 -0700

    updated hero in about me

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,11 +9,17 @@ class about extends Component {
             <div>
                 <section class="hero2">
                     <div class="hero-inner2">
-                        <h1>About Me</h1>
-                        <h2>I'm a Front-End developer based in Los Angeles, California</h2>
+                        <div className="aboutBox">
+                            <h1 className="aboutTitle">About Me</h1>
+                            <h2>I'm a Front-End developer based in Los Angeles, California</h2>
+                            <span className="aboutSpan">I'm an energetic and ambitious developer with a focus on fun, attractive, user-friendly web applications. 
+                                I thrive on challenges to enhance my own creativity, critical thinking and understanding of development 
+                                through implementation and exploration.
+                            </span>
+                        </div>
                     </div>
                 </section>
-                <main>
+                <main className="main">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nibh molestie, efficitur leo sed, viverra nunc. Donec vehicula accumsan erat facilisis ullamcorper. Donec commodo quis dui nec placerat. Donec mi orci, scelerisque eget nisl ac, hendrerit condimentum odio. Nam dictum odio eget quam tempus, a mattis odio ornare. Nullam auctor libero ut libero suscipit, ut accumsan nunc condimentum. Donec ullamcorper maximus sapien quis egestas.</p>
                     <p>Mauris viverra scelerisque lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ultrices enim sit amet elit tincidunt maximus. Suspendisse vitae pellentesque lectus. Duis commodo leo suscipit augue mollis, non venenatis dolor ullamcorper. Duis tincidunt scelerisque lacus, vel vehicula leo consectetur vel. Duis posuere nisl non odio consequat ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     <p>Etiam a leo nec mi blandit euismod. Etiam fringilla odio vitae risus ornare, id bibendum velit consequat. Fusce posuere risus sollicitudin condimentum ultrices. Fusce gravida, purus eget laoreet mattis, velit sapien ultrices diam, id dapibus erat leo id quam. Maecenas quis risus convallis, placerat elit non, iaculis tortor. Nullam porttitor magna risus, quis bibendum metus tincidunt in. Etiam vel ligula ac risus mattis tincidunt vel sit amet ante. Morbi et viverra ligula. Ut ac dignissim nisi, condimentum imperdiet mauris. Pellentesque ut ipsum vel diam tristique faucibus eu et lectus. Maecenas posuere neque non lacus bibendum, sit amet pharetra justo semper. Sed mi risus, tempor sit amet ligula eget, varius pretium est. Sed a odio in orci accumsan pretium suscipit ut quam.</p>

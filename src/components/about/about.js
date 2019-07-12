65e6cb620098e5d86cd55046503c02c7283405aa
Robert Cortez <Nuhuhfoo@gmail.com>
2019-07-11 17:15:20 -0700

    updated about me page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,7 +9,7 @@ class about extends Component {
         return(
             <div>
                 <div className="container2">
-                    <div className="box2 about">
+                    <div className="box2">
                         <h1 className="aboutTitle">About Me</h1>
                         <h2 className="aboutSubTitle">I'm a Front-End developer based in Los Angeles, California</h2>
                         <span className="aboutSpan">I'm an energetic and ambitious developer with a focus on fun, attractive, user-friendly web applications. 
@@ -17,27 +17,13 @@ class about extends Component {
                             through implementation and exploration.
                         </span>
                     </div>
-                    <div className="box2">
+                    <div className="box2 boxImage">
                         <img className="me" src={Me} alt="Me"/>
                     </div>
                 </div>
-
-                {/* <section class="hero2">
-                    <div class="hero-inner2">
-                        <div className="aboutBox">
-                            <h1 className="aboutTitle">About Me</h1>
-                            <h2>I'm a Front-End developer based in Los Angeles, California</h2>
-                            <span className="aboutSpan">I'm an energetic and ambitious developer with a focus on fun, attractive, user-friendly web applications. 
-                                I thrive on challenges to enhance my own creativity, critical thinking and understanding of development 
-                                through implementation and exploration.
-                            </span>
-                        </div>
-                    </div>
-                </section>
                 <main className="main">
             
                 </main>
-                <img className="me" src={Me} alt="Me"/> */}
             </div>
         )
     }

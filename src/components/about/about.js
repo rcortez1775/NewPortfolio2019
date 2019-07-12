f397050e18362a3fd96ec867a1858394300ad0a6
Robert Cortez <Nuhuhfoo@gmail.com>
2019-07-12 12:52:46 -0700

    updated 2nd part of about me page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,7 +2,6 @@ import { Component } from "react";
 import React from 'react';
 import './about.css';
 import Me from '../../images/halo.png';
-import Circuit from '../../images/circuit.png';
 
 
 class about extends Component {
@@ -10,8 +9,8 @@ class about extends Component {
         return(
             <div>
                 <div className="container2">
-                    <div className="box2">
-                        <h1 className="aboutTitle">&lt;About Me/&gt;</h1>
+                    <div className="box2 boxAbout">
+                        <h1 className="aboutTitle">About Me</h1>
                         <h2 className="aboutSubTitle">I'm a Front-End developer based in Los Angeles, California</h2>
                         <span className="aboutSpan">I'm an energetic and ambitious developer with a focus on fun, attractive, user-friendly web applications. 
                             I thrive on challenges to enhance my own creativity, critical thinking and understanding of development 
@@ -24,10 +23,24 @@ class about extends Component {
                 </div>
                 <div>
                 <main className="main">
-                    <div>
-                        <h3 className="myEdu">&emsp;&emsp;EDUCATION&emsp;&emsp;</h3>
-                        <img className="circuit" src={Circuit} alt="Circuit"/>
-                    </div>
+                    {/* <div className="myEduBox">
+                        <span className="myEdu">EDUCATION</span>
+                        <p>Call me Ishmael. Some years ago- never mind how long precisely- having little or no money in my purse, and nothing particular to interest me on shore, 
+                            I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. 
+                            Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before 
+                            coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a 
+                            strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off- then, I account it high time 
+                            to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship.
+                        </p>
+                    </div> */}
+
+<div class="moby">
+  <h2 className="myEdu">EDUCATION</h2>
+<p>Call me Ishmael. Some years ago- never mind how long precisely- having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off- then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.</p>
+</div>
+
+
+
                 </main>
                 </div>
             </div>

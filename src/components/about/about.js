61ca75e7f02a90f3bf5b9b61658e19daf4f71d95
Robert Cortez <Nuhuhfoo@gmail.com>
2019-07-12 19:41:00 -0700

    updated about me page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import React from 'react';
 import './about.css';
 import Me from '../../images/halo.png';
+import Rifle from '../../images/rifleExpert.png';
 
 
 class about extends Component {
@@ -36,15 +37,18 @@ class about extends Component {
                         <p className="description">drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off- then, I account it high time to get to sea as soon as I can.</p>
                     </div>
                 </main>
-                {/* <div className="container3">
-<h1>HHSDJFHSKJDFGJKSGDKJFHGSKJHDF</h1>
-<h1>HHSDJFHSKJDFGJKSGDKJFHGSKJHDF</h1>
-<h1>HHSDJFHSKJDFGJKSGDKJFHGSKJHDF</h1>
-<h1>HHSDJFHSKJDFGJKSGDKJFHGSKJHDF</h1>
-<h1>HHSDJFHSKJDFGJKSGDKJFHGSKJHDF</h1>
-<h1>HHSDJFHSKJDFGJKSGDKJFHGSKJHDF</h1>
-
-                </div> */}
+                <div className="container3">
+                <div className="box2 rifleImage">
+                        <img className="rifle" src={Rifle} alt="Rifle"/>
+                    </div>
+                <div className="box2 milBox">
+                        <h1 className="milTitle">Military Service</h1>
+                        <h2 className="aboutSubTitle">United States Marine Corps</h2>
+                        <span className="aboutSpan">
+                        In addition to maintaining log books, preparing reports and logging records, directives and correspondence for military aircraft and weaponry, I was trained in data processing military equipment. This allowed me to oversee and supervise maintenance administration functions, as well as set up, operate and maintain logistics hardware and systems.
+                        </span>
+                    </div>
+                </div>
                 </div>
             </div>
         )

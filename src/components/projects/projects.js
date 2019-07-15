098c6d4223b1fee78eedc9433b245b00a27aaf22
Robert Cortez <Nuhuhfoo@gmail.com>
2019-07-14 22:42:49 -0700

    edited projects page and removed images from about background to improve loadtime

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,11 +1,24 @@
 import { Component } from "react";
 import React from 'react';
+import './projects.css';
+import Pup from '../../images/keepPUP.jpg';
 
 
 class Projects extends Component {
     render() {  
         return(
-            <div><p>Projects</p></div>
+            <div>
+                <div className="projectContainer">
+                    <div className="box3">
+                        <p className="appTitle">keepPUP App</p>
+                        <p className="appDescription">The keepPUP App allows users to keep up with their furry friends. Track when you feed and walk your dog with push notifications blah blah blah</p>
+                    </div>
+
+                    <div className="box3">
+                        <img className="pup" src={Pup} alt="Pup"/>
+                    </div>
+                </div>
+            </div>
         )
     }
 }

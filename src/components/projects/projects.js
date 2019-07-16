6533ee470869e9f7b4f79e056827de8ef021b7e4
Robert Cortez <Nuhuhfoo@gmail.com>
2019-07-16 08:14:53 -0700

    updated projects bio

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -13,6 +13,7 @@ class Projects extends Component {
                     <div className="box3 keepPUPBox">
                         <p className="appTitle">keepPUP</p>
                         <p className="appDescription">The keepPUP App allows users to keep up with their furry friends. Track when you feed and walk your dog with push notifications blah blah blah</p>
+                        <p className="appDescription"><span className="boldText">Made with:</span> JavaScript, Node.js, Express, MySQL and Material-UI</p>
                     </div>
 
                     <div className="box3 keepPUPBox">
@@ -24,10 +25,11 @@ class Projects extends Component {
                     <div className="box3 keepPUPBox">
                         <img className="codeCard" src={CodeCard} alt="Codecard"/>
                     </div>
-                    
+
                     <div className="box3 keepPUPBox">
                         <p className="appTitle">myCodeCard</p>
                         <p className="codeDescription">myCodeCard is an app where users can store related data and information on subjects learned. blah blah blah blah blah</p>
+                        <p className="appDescription"><span className="boldText">Made with:</span> React.JS, MongoDB, Express, Node.js and Material-UI</p>
                     </div>
                 </div>
 

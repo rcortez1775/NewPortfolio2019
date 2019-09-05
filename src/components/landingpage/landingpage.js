4b3a3801cf051f34d3897dde9a64ce264052ff71
Robert Cortez <Nuhuhfoo@gmail.com>
2019-09-04 20:25:45 -0700

    updated skills cards on landing page

--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -23,45 +23,66 @@ class LandingPage extends Component {
                         <p className="textSkills">&emsp;&emsp;SOME OF MY SKILLS&emsp;&emsp;</p>
                     </div>
                     <div className="container">
-                        <div className="box">
-                            <div className="card">
-                                <span className="boxTitle">FOUNDATION</span>
-                                <br></br>
-                                <span className="boxList">ES6 JAVASCRIPT</span>
-                                <br></br>
-                                <span className="boxList">HTML(5)</span>
-                                <br></br>
-                                <span className="boxList">CSS(3)</span>
-                                <br></br>
-                                <span className="boxList">GIT</span>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <p className="boxTitle">FOUNDATION</p>
+                                </div>  
+                                <div class="flip-card-back">
+                                    <div className="pushDwn">
+                                        <span className="boxList">ES6 JAVASCRIPT</span>
+                                        <br></br>
+                                        <span className="boxList">NIGHTMARE</span>
+                                        <br></br>
+                                        <span className="boxList">HTML(5)</span>
+                                        <br></br>
+                                        <span className="boxList">CSS(3)</span>
+                                        <br></br>
+                                        <span className="boxList">JEST</span>
+                                        <br></br>
+                                        <span className="boxList">GIT</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="card">
-                                <span className="boxTitle">LIBRARIES</span>
-                                <br></br>
-                                <span className="boxList">MATERIAL-UI</span>
-                                <br></br>
-                                <span className="boxList">BOOTSTRAP</span>
-                                <br></br>
-                                <span className="boxList">REACT.JS</span>
-                                <br></br>
-                                <span className="boxList">jQUERY</span>
+
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front two">
+                                    <p className="boxTitle">LIBRARIES</p>
+                                </div>
+                                <div class="flip-card-back two">
+                                    <div className="pushDwn">
+                                        <span className="boxList">MATERIAL-UI</span>
+                                        <br></br>
+                                        <span className="boxList">BOOTSTRAP</span>
+                                        <br></br>
+                                        <span className="boxList">REACTJS</span>
+                                        <br></br>
+                                        <span className="boxList">jQUERY</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="card">
-                                <span className="boxTitle">SERVER-SIDE</span>
-                                <br></br>
-                                <span className="boxList">MONGODB</span>
-                                <br></br>
-                                <span className="boxList">EXPRESS</span>
-                                <br></br>
-                                <span className="boxList">NODE.JS</span>
-                                <br></br>
-                                <span className="boxList">HEROKU</span>
-                                <br></br>
-                                <span className="boxList">MYSQL</span>
+
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front three">
+                                    <p className="boxTitle">SERVER-SIDE</p>
+                                </div>
+                                <div class="flip-card-back three">
+                                    <div className="pushDwn">
+                                        <span className="boxList">MONGODB</span>
+                                        <br></br>
+                                        <span className="boxList">EXPRESS</span>
+                                        <br></br>
+                                        <span className="boxList">NODEJS</span>
+                                        <br></br>
+                                        <span className="boxList">HEROKU</span>
+                                        <br></br>
+                                        <span className="boxList">MYSQL</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>

c63fd886b7bf6b28651c6de30e56cb8992cf0e86
Robert Cortez <Nuhuhfoo@gmail.com>
2019-10-08 13:06:18 -0700

    updated landingpage

--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -1,6 +1,11 @@
 import { Component } from "react";
 import React from 'react';
 import './landingpage.css';
+import Foundation from '../../images/foundation.png';
+import Library from '../../images/library.png';
+import Server from '../../images/database.png';
+
+
 
 
 class LandingPage extends Component {
@@ -11,79 +16,65 @@ class LandingPage extends Component {
                     <div className="hero-inner">
                         <p className="name">Robert Cortez</p>
                         <p className="intro">Developer + Gamer + Dog Lover</p>
-                        <div class="arrow">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
                     </div>
                 </section>
-                <main>
+                <main className="main">
                     <div>
                         <p className="textSkills">&emsp;&emsp;SOME OF MY SKILLS&emsp;&emsp;</p>
                     </div>
                     <div className="container">
-                        <div class="flip-card">
-                            <div class="flip-card-inner">
-                                <div class="flip-card-front foundationCard">
-                                    <p className="boxTitle">FOUNDATION</p>
-                                </div>  
-                                <div class="flip-card-back">
-                                    <div className="pushDwn">
-                                        <span className="boxList">ES6 JAVASCRIPT</span>
-                                        <br></br>
-                                        <span className="boxList">NIGHTMARE</span>
-                                        <br></br>
-                                        <span className="boxList">HTML(5)</span>
-                                        <br></br>
-                                        <span className="boxList">CSS(3)</span>
-                                        <br></br>
-                                        <span className="boxList">JEST</span>
-                                        <br></br>
-                                        <span className="boxList">GIT</span>
-                                    </div>
-                                </div>
+                        <div class="cardBox">
+                            <div class="iconBox">
+                                <img className="skillsIcon" src={Foundation} alt="Foundation"/>
+                                <p className="boxTitle">FOUNDATION</p>
+                            </div>  
+                            <div className="boxCont">
+                                <span className="boxList">ES6 JAVASCRIPT</span>
+                                <br></br>
+                                <span className="boxList">NIGHTMARE</span>
+                                <br></br>
+                                <span className="boxList">HTML(5)</span>
+                                <br></br>
+                                <span className="boxList">CSS(3)</span>
+                                <br></br>
+                                <span className="boxList">JEST</span>
+                                <br></br>
+                                <span className="boxList">GIT</span>
+                            </div>      
+                        </div>
+
+                        <div class="cardBox">
+                            <div class="iconBox">
+                                <img className="skillsIcon" src={Library} alt="Library"/>
+                                <p className="boxTitle">LIBRARIES</p>
+                            </div>
+                            <div class="boxCont">
+                                <span className="boxList">MATERIAL-UI</span>
+                                <br></br>
+                                <span className="boxList">BOOTSTRAP</span>
+                                <br></br>
+                                <span className="boxList">REACTJS</span>
+                                <br></br>
+                                <span className="boxList">jQUERY</span>
                             </div>
                         </div>
 
-                        <div class="flip-card">
-                            <div class="flip-card-inner">
-                                <div class="flip-card-front libraryCard">
-                                    <p className="boxTitle">LIBRARIES</p>
-                                </div>
-                                <div class="flip-card-back two">
-                                    <div className="pushDwn">
-                                        <span className="boxList">MATERIAL-UI</span>
-                                        <br></br>
-                                        <span className="boxList">BOOTSTRAP</span>
-                                        <br></br>
-                                        <span className="boxList">REACTJS</span>
-                                        <br></br>
-                                        <span className="boxList">jQUERY</span>
-                                    </div>
-                                </div>
+                        <div class="cardBox">
+                            <div class="iconBox">
+                                <img className="skillsIcon" src={Server} alt="Foundation"/>
+                                <p className="boxTitle">SERVER-SIDE</p>
                             </div>
-                        </div>
-
-                        <div class="flip-card">
-                            <div class="flip-card-inner">
-                                <div class="flip-card-front serverCard">
-                                    <p className="boxTitle">SERVER-SIDE</p>
-                                </div>
-                                <div class="flip-card-back three">
-                                    <div className="pushDwn">
-                                        <span className="boxList">MONGODB</span>
-                                        <br></br>
-                                        <span className="boxList">EXPRESS</span>
-                                        <br></br>
-                                        <span className="boxList">NODEJS</span>
-                                        <br></br>
-                                        <span className="boxList">HEROKU</span>
-                                        <br></br>
-                                        <span className="boxList">MYSQL</span>
-                                    </div>
-                                </div>
-                            </div>
+                            <div className="boxCont">
+                                <span className="boxList">MONGODB</span>
+                                <br></br>
+                                <span className="boxList">EXPRESS</span>
+                                <br></br>
+                                <span className="boxList">NODEJS</span>
+                                <br></br>
+                                <span className="boxList">HEROKU</span>
+                                <br></br>
+                                <span className="boxList">MYSQL</span>
+                            </div>  
                         </div>
                     </div>
                 </main>

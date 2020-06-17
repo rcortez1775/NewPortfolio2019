a44fe7ba5f619e5d7cd77851811028550998af97
Robert Cortez <Nuhuhfoo@gmail.com>
2020-06-17 14:21:58 -0700

    updates to about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -19,49 +19,37 @@ class about extends Component {
                 <div className="boyBox">
                     <img className="boy" src={Boy} alt="boy"/>
                 </div>
-
-            
-
-
-
-
-
-                    <div className="main">
-                        {/* <div class className="aboutMeDiv">
-                            
-                            <p className="aboutMe">I’m Robert. A UI/UX Designer and Developer based in Los Angeles. I’m passionately curious about learning how people’s thought processes and feelings drive the decisions they make.</p>
-                            <p className="aboutMe sec">My goal as a designer & developer is to create meaningful and engaging experiences that are easily accessible and functional. To learn and test new concepts and ideas that will allow me to grow both professionally and personally.</p>
-                        </div> */}
-                        <div className="pushUP">
-                            <p className="education textSkills">&emsp;&emsp;EDUCATION&emsp;&emsp;</p>
+                <div className="main">
+                    <div className="pushUP">
+                        <p className="education textSkills">&emsp;&emsp;EDUCATION&emsp;&emsp;</p>
+                    </div>
+                    <div class="container">
+                        <div className="cardBox mobileBox">
+                            <div className="boxCont">
+                                <img className="graduate" src={Star} alt="chevron"/>
+                                <p className="eduTitle">UC Irvine Coding Bootcamp</p>
+                                <p className="date">4.2018 - 7.2018</p>
+                                <p className="description">The coding bootcamp at UC Irvine was an immersive 12-week program. The course covered Computer Science applied to Javascript, browser based technologies, Databases and Deployment and server side development. The program was filled with so many intellectually curious individuals with diverse tech backgrounds.</p>
+                            </div>
                         </div>
-                        <div class="container">
-                            <div className="cardBox mobileBox">
-                                <div className="boxCont">
-                                    <img className="graduate" src={Star} alt="chevron"/>
-                                    <p className="eduTitle">UC Irvine Coding Bootcamp</p>
-                                    <p className="date">4.2018 - 7.2018</p>
-                                    <p className="description">The coding bootcamp at UC Irvine was an immersive 12-week program. The course covered Computer Science applied to Javascript, browser based technologies, Databases and Deployment and server side development. The program was filled with so many intellectually curious individuals with diverse tech backgrounds.</p>
-                                </div>
+                        <div className="cardBox mobileBox">
+                            <div className="boxCont">
+                                <img className="graduate" src={Star} alt="chevron"/>
+                                <p className="eduTitle">Sabio Coding Bootcamp</p>
+                                <p className="date">1.2018 - 2.2018</p>
+                                <p className="description">The coding bootcamp at Sabio was a one month pre-work course that prepared students for an extensive 12 week course. The program highlighted the front-end and back-end technologies. I would have attended their 12 week program but found the curriculum at UC Irvine to be a bit more robust.</p>
                             </div>
-                            <div className="cardBox mobileBox">
-                                <div className="boxCont">
-                                    <img className="graduate" src={Star} alt="chevron"/>
-                                    <p className="eduTitle">Sabio Coding Bootcamp</p>
-                                    <p className="date">1.2018 - 2.2018</p>
-                                    <p className="description">The coding bootcamp at Sabio was a one month pre-work course that prepared students for an extensive 12 week course. The program highlighted the front-end and back-end technologies. I would have attended their 12 week program but found the curriculum at UC Irvine to be a bit more robust.</p>
-                                </div>
-                            </div>
-                            <div className="cardBox mobileBox">
-                                <div className="boxCont">
-                                    <img className="graduate" src={Star} alt="chevron"/>
-                                    <p className="eduTitle">CodeAcademy & Udemy</p>
-                                    <p className="date">11.2016 - Current</p>
-                                    <p className="description">Both CodeAcademy and Udemy were my first introduction into programming. Lessons covered ranged from browser based technologies to server-side development. What I enjoy from these two platforms is the ease of accessibility and materials covered.</p>
-                                </div>
+                        </div>
+                        <div className="cardBox mobileBox">
+                            <div className="boxCont">
+                                <img className="graduate" src={Star} alt="chevron"/>
+                                <p className="eduTitle">CodeAcademy & Udemy</p>
+                                <p className="date">11.2016 - Current</p>
+                                <p className="description">Both CodeAcademy and Udemy were my first introduction into programming. Lessons covered ranged from browser based technologies to server-side development. What I enjoy from these two platforms is the ease of accessibility and materials covered.</p>
                             </div>
                         </div>
                     </div>
+                </div>
                     <div className="container3">
                         <p className="education textSkills one">&emsp;&emsp;MILITARY&emsp;&emsp;</p>
                         <div className="milService">
--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -1,10 +1,6 @@
 import { Component } from "react";
 import React from 'react';
 import './landingpage.css';
-// import Foundation from '../../images/foundation.png';
-// import Library from '../../images/library.png';
-// import Server from '../../images/database.png';
-// import Graphic from '../../images/graphic.png';
 import Robert from '../../images/robertc.png';
 import Linkedin from '../../images/linkedin.png';
 import Git from '../../images/git.png';
@@ -21,83 +17,12 @@ class LandingPage extends Component {
                     <img className="myLogo" src={Robert} alt="icon"/>
                     <p className="name">Robert Cortez</p>
                     <p className="intro">Developer + Designer</p>
-                    <div className="iconBox">
+                    <div>
                         <img className="icon" id="iconHover" src={Git} alt="Git" onClick={() => window.open("https://github.com/rcortez1775", "_blank")}/> 
                         <img className="icon" id="iconHover" src={Gmail} alt="Gmail" onClick={() => window.open("https://github.com/rcortez1775", "_blank")}/> 
                         <img className="icon" id="iconHover" src={Linkedin} alt="Linkedin" onClick={() => window.open("https://www.linkedin.com/in/robert-cortez-bb5537a5/", "_blank")}/>
                     </div>
                 </div>
-                
-                {/* <div className="main">
-                    <div className="container">
-                        <div class="cardBox">
-                            <div class="iconBox">
-                                <img className="skillsIcon" src={Foundation} alt="Foundation"/>
-                                <p className="boxTitle">Foundation</p>
-                            </div> 
-                            <div className="boxCont">
-                                <span className="boxList">ES6 JavaScript</span>
-                                <br></br>
-                                <span className="boxList">Nightmare</span>
-                                <br></br>
-                                <span className="boxList">HTML(5)</span>
-                                <br></br>
-                                <span className="boxList">CSS(3)</span>
-                                <br></br>
-                                <span className="boxList">Jest</span>
-                                <br></br>
-                                <span className="boxList">Git</span>
-                            </div>      
-                        </div>
-                        <div class="cardBox">
-                            <div class="iconBox">
-                                <img className="skillsIcon" src={Library} alt="Library"/>
-                                <p className="boxTitle">Libraries</p>
-                            </div>
-                            <div class="boxCont">
-                                <span className="boxList">Material-UI</span>
-                                <br></br>
-                                <span className="boxList">Bootstrap</span>
-                                <br></br>
-                                <span className="boxList">ReactJS</span>
-                                <br></br>
-                                <span className="boxList">jQuery</span>
-                            </div>
-                        </div>
-
-                        <div class="cardBox">
-                            <div class="iconBox">
-                                <img className="skillsIcon" src={Server} alt="Foundation"/>
-                                <p className="boxTitle">Server-Side</p>
-                            </div>
-                            <div className="boxCont">
-                                <span className="boxList">MongoDB</span>
-                                <br></br>
-                                <span className="boxList">Express</span>
-                                <br></br>
-                                <span className="boxList">NodeJS</span>
-                                <br></br>
-                                <span className="boxList">Heroku</span>
-                                <br></br>
-                                <span className="boxList">MySql</span>
-                            </div>  
-                        </div>
-
-                        <div class="cardBox">
-                            <div class="iconBox">
-                                <img className="skillsIcon" src={Graphic} alt="Graphic"/>
-                                <p className="boxTitle">UI/UX</p>
-                            </div>
-                            <div className="boxCont">
-                                <span className="boxList">MindManager</span>
-                                <br></br>
-                                <span className="boxList">Balsamiq</span>
-                                <br></br>
-                                <span className="boxList">Figma</span>
-                            </div>  
-                        </div>
-                    </div>
-                </div> */}
             </div>
         )
     }

be0d8285896bf099f187486124e1afe5fd08f968
Robert Cortez <Nuhuhfoo@gmail.com>
2020-07-02 21:20:45 -0700

    updates

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,7 +3,8 @@ import React from 'react';
 import './projects.css';
 // import Therabody from '../../images/therabody.png';
 // import KeepPUP from '../../images/keepup.png';
-
+import Phone from '../../images/smartphone.png';
+import Comp from '../../images/computer.png';
 
 class Projects extends Component {
     render() {
@@ -15,21 +16,31 @@ class Projects extends Component {
                     <input type="radio" name="slider" title="slide2" class="slider__nav" />
                     <input type="radio" name="slider" title="slide3" class="slider__nav" />
                     <input type="radio" name="slider" title="slide4" class="slider__nav" />
+                    <input type="radio" name="slider" title="slide5" class="slider__nav" />
                     <div class="slider__inner">
-                        <div class="slider__contents"><i class="slider__image fa fa-codepen"></i>
+                        <div class="slider__contents">
+                            <img src={Phone} alt="smartPhone" className="projectIcon"/>
                             <h2 class="slider__caption">Therabody</h2>
                             <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
                         </div>
-                        <div class="slider__contents"><i class="slider__image fa fa-newspaper-o"></i>
-                            <h2 class="slider__caption">newspaper-o</h2>
+                        <div class="slider__contents">
+                            <img src={Phone} alt="smartPhone" className="projectIcon"/>
+                            <h2 class="slider__caption">KeepPUP</h2>
                             <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
                         </div>
-                        <div class="slider__contents"><i class="slider__image fa fa-television"></i>
-                            <h2 class="slider__caption">television</h2>
+                        <div class="slider__contents">
+                            <img src={Comp} alt="Computer" className="projectIcon"/>
+                            <h2 class="slider__caption">MyCodeCard</h2>
                             <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
                         </div>
-                        <div class="slider__contents"><i class="slider__image fa fa-diamond"></i>
-                            <h2 class="slider__caption">diamond</h2>
+                        <div class="slider__contents">
+                            <img src={Comp} alt="Computer" className="projectIcon"/>
+                            <h2 class="slider__caption">Circle of Friends</h2>
+                            <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+                        </div>
+                        <div class="slider__contents">
+                            <img src={Comp} alt="Computer" className="projectIcon"/>
+                            <h2 class="slider__caption">DoomsDay Database</h2>
                             <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
                         </div>
                     </div>

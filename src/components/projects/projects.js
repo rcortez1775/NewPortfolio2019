bfc9425c41c698a8a6dcd57fbf0783a2184433ec
Robert Cortez <Nuhuhfoo@gmail.com>
2020-07-03 11:53:41 -0700

    updates

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -5,6 +5,9 @@ import Therabody from '../../images/therabody.png';
 import KeepPUP from '../../images/keepup.png';
 import Phone from '../../images/smartphone.png';
 import Comp from '../../images/computer.png';
+import CodeCard from '../../images/codecard.png';
+import Circle from '../../images/circle.png';
+import Doom from '../../images/doomsday.png';
 
 class Projects extends Component {
     render() {
@@ -33,16 +36,19 @@ class Projects extends Component {
                         <div class="slider__contents">
                             <img src={Comp} alt="Computer" className="projectIcon"/>
                             <h2 class="slider__caption">MyCodeCard</h2>
+                            <img src={CodeCard} alt="CodeCard" className="projectComp"/>
                             <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
                         </div>
                         <div class="slider__contents">
                             <img src={Comp} alt="Computer" className="projectIcon"/>
                             <h2 class="slider__caption">Circle of Friends</h2>
+                            <img src={Circle} alt="CircleofFriends" className="projectComp"/>
                             <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
                         </div>
                         <div class="slider__contents">
                             <img src={Comp} alt="Computer" className="projectIcon"/>
                             <h2 class="slider__caption">DoomsDay Database</h2>
+                            <img src={Doom} alt="doomsday" className="projectComp"/>
                             <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
                         </div>
                     </div>
